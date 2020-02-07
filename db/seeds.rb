72aa67d13a51e29c0662d434bde598f6023dc3ee
jeferbc <jeffe.bernal@gmail.com>
2020-02-07 10:10:34 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Producto 1", price: 19)
+Product.create(name: "Producto 2", price: 23) 
