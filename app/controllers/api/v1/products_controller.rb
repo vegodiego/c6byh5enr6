17a98bcdcdfa0d478158b3aa117f98a700ea1329
root <root@LAPTOP-8DE7N6T3.localdomain>
2020-03-14 15:48:49 -0500

    function createJSON

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,4 +5,13 @@ class Api::V1::ProductsController < ApplicationController
     render json: products, status: 200
   end
 
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      render json: product
+    else
+      render json: { errors: @product.errors }, status: 422
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, :defaults => {:format => :json}  do
     namespace :v1 do
-      resources :products, :only => [:index]
+      resources :products, :only => [:index, :create, :update, :destroy]
     end
   end
   resources :products
