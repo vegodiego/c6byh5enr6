2a119b340b3d5c47d5d1acd6bddeb6e25e7f62a8
root <root@LAPTOP-8DE7N6T3.localdomain>
2020-03-15 19:53:13 -0500

    implementing functions destroy and update

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -14,6 +14,22 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product, status: 200
+    else
+      render json: { errors: @product.errors }, status: 422
+    end
+    
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    head 204
+  end
+
   private
     def product_params
       params.require(:product).permit(:name, :price)
