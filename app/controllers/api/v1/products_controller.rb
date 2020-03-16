1b39e463ac272ac74323cb7b9b7c0f770abee223
root <root@LAPTOP-8DE7N6T3.localdomain>
2020-03-15 19:00:03 -0500

    add validations

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.create(product_params)
+    @product = Product.new(product_params)
     if @product.save
-      render json: product
+      render json: @product, status: 201
     else
       render json: { errors: @product.errors }, status: 422
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :price, presence: true 
+    validates :name, presence: true
 end
